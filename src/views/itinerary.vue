fac1b67c8be719c994c45d46b91065ec44d023fa
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2023-10-16 10:34:29 +0800

    edit navbar in app.vue

--- src/views/itinerary.vue
+++ src/views/itinerary.vue
@@ -30,41 +30,6 @@
 
 
 <template>
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #05c8f7;"
-    aria-label="Second navbar example">
-    <div class="container-fluid">
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02"
-        aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExample02">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-black" href="Tools/Toolpage.html">Tools</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="billbuddy.html">BillBuddy</a>
-          </li>
-        </ul>
-        <div class="position-absolute top-0 start-50 translate-middle-x">
-          <a href="#"><img src="logo\itiner-easy.svg"  style="width: 100px; height: 100px; margin-top: -15px;"
-            alt="itiner-easy logo"></a>
-        </div>
-        <ul class="navbar-nav pull-right">
-          <li class="nav-item">
-            <a class="nav-link rounded"
-              style="background-color: white; color: #05c8f7; padding-left: 20px; padding-right: 20px;"
-              href="#">Profile</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
 
 
 <div class="Mainbody">
@@ -349,7 +314,7 @@ function SearchMuseums(city) {
         }
     });
     }
-nction searchgardens(city) {
+function searchgardens(city) {
     var city = document.getElementById("country").value;
 
     const request = {
