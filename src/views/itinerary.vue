ae5e1fa04aa6a4cf2e13553fa7575c46463c6111
songdre123 <129580329+songdre123@users.noreply.github.com>
2023-10-16 15:26:35 +0800

    updates

--- src/views/itinerary.vue
+++ src/views/itinerary.vue
@@ -52,7 +52,7 @@
           </li>
         </ul>
         <div class="position-absolute top-0 start-50 translate-middle-x">
-          <a href="#"><img src="logo\itiner-easy.svg"  style="width: 100px; height: 100px; margin-top: -15px;"
+          <a href="#"><img src="../components/logo/itiner-easy.svg"  style="width: 100px; height: 100px; margin-top: -15px;"
             alt="itiner-easy logo"></a>
         </div>
         <ul class="navbar-nav pull-right">
@@ -70,54 +70,74 @@
 <div class="Mainbody">
 
 
-    <h7>Which City do you want to go to?</h7><input type="text" id="country" name="City">
+    <h7>Which City do you want to go to?</h7><input type="text" id="country" name="City"  v-model="town"  class="form-control input-sm" placeholder="Where are you going?" required>"
     <div class="slidecontainer">
         For many days?
-        <input id="slider" type="range" min="1" max="3" step="1" @input="sliderChange(this.value)">
-        <div class="SliderValue">
-
-        </div>
-        <output id="sliderVal">
-            2
-        </output>  
+        <div>
+    <input type="range" min="1" max="3" step="1" v-model="sliderValue" @input="sliderChange" required />
+    <p>Days: {{ sliderValue }}</p>
+        </div> 
     
     </div>
 
 
 
-    <h6>What kind of person are you?</h6>
-<br>
-<input type="radio" id="Indoor" name="outgoing" value="Indoor">
+    What kind of person are you?<br>
+<input type="radio" id="Indoor" value="Indoor" v-model="outgoing" required>
 <label for="Indoor">Indoor</label>
-<input type="radio" id="Outdoor" name="outgoing" value="Outdoor">
+<input type="radio" id="Outdoor"  value="Outdoor" v-model="outgoing" required>
 <label for="Outdoor">Outdoor</label>
-<input type="radio" id="Both" name="outgoing" value="Both">
+<input type="radio" id="Both"  value="Both" v-model="outgoing" required >
 <label for="Both">I'm fine with either!</label>
 
 
 <br>
-    How will you be getting around?<br> <input type="radio" id="car" name="Transport" value="DRIVING">
+    How will you be getting around?<br>
+     <input type="radio" id="car" value="DRIVING" v-model="transport" required>
     <label for="car">Car</label>
-    <input type="radio" id="Public Transport" name="Transport" value="TRANSIT">
+    <input type="radio" id="Public Transport" value="TRANSIT"  v-model="transport" required>
     <label for="Public Transport">Public Transport</label>
-    <input type="radio" id="Cycling" name ="Transport" value = "BICYCLING">
+    <input type="radio" id="Cycling" value = "BICYCLING"  v-model="transport" required>
     <label for="Cycling">Bicycle</label>
-    <input type="radio" id="walking" name="Transport" value="WALKING">
+    <input type="radio" id="walking" value="WALKING"  v-model="transport" required>
     <label for="walking">Walking</label>
 
 <br>
-    <button @click="generateitinerary">Generate Itinerary</button>
+    <button @click="checkempty">Generate Itinerary</button>
+
+</div>
+<div >
+
+</div>
+<div id ="results">
 
 </div>
 
 </template>
 
 <script>
+import axios from 'axios'; // Import Axios
 
-async function getweather() {
+export default {
+  data() {
+    return {
+      sliderValue: 1,
+      selectedOption: null, // Initially no option is selected
+      town: "",
+      outgoing: "",
+      transport: "",
+    };
+  },
+    methods: {
+        sliderChange(event) {
+        this.sliderValue = event.target.value;
+        },
+
+
+async getweather() {
     return new Promise(async (resolve, reject) => {
-        var city = document.getElementById("country").value;
-        var days = document.getElementById("sliderVal").value;
+        var city = this.town;
+        var days = this.sliderValue;
 
         try {
             var weatherkey ="cfb27632a44746f6aaf01356231409";
@@ -139,25 +159,73 @@ async function getweather() {
             reject(error);
         }
     });
-}
+},
+
+ searchBothAttractions(city) {
+    var city = this.town;
+
+    const request = {
+        query: `Tourist Attractions in ${city}`,
+        fields: ['name', 'formatted_address','types', 'business_status', 'location'],
+    };
 
 
 
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+    const outdoorplaces = ['park','zoo','amusement_park',''];
+    // const outdoo
 
-  async function generateitinerary() {
+    service.textSearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            result = [];
+        for (const place of results) {
+
+            console.log(`Name: ${place.name}`);
+            console.log(`Address: ${place.formatted_address}`);
+            console.log(`Types: ${place.types}`);
+            console.log(`Business Status: ${place.business_status}`);
+            console.log(`Location: ${place.geometry.location}`);
+            console.log('---');
+            result.push(place.name);
+        }
+        //place results in checkbox
+        for (var i = 0; i < result.length; i++) {
+            var checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.name = "name";
+            checkbox.value = "value";
+            checkbox.id = "id";
+
+            var label = document.createElement('label')
+            label.htmlFor = "id";
+            label.appendChild(document.createTextNode(result[i]));
+
+            document.getElementById('selectplaces').appendChild(checkbox);
+            document.getElementById('selectplaces').appendChild(label);
+            document.getElementById('selectplaces').appendChild(document.createElement("br"));
+        }
+        }
+         else {
+        console.error(`Error: ${status}`);
+        }
+    });
+    },
+
+
+  async generateitinerary() {
     try {
-        var weather = await getweather(); // Wait for getweather to finish
+        var weather = await this.getweather(); // Wait for getweather to finish
         console.log(weather);
-        var preferences = document.querySelector('input[name="outgoing"]:checked').value;
-        var transport = document.querySelector('input[name="Transport"]:checked').value;
+        var preferences = this.outgoing;
+        var transport = this.transport;
         var usedattractions = [];
         var itinerary = [];
-        var city = document.getElementById("country").value;
-        var days = document.getElementById("sliderVal").value;
+        var city = this.town;
+        var days = this.sliderValue;
 
         for (var i = 0; i < weather.length; i++) {
             if (weather[i].indexOf("Rain") >= 0 || weather[i].indexOf("hazy") >= 0) {
-                var all_attractions = searchIndoorAttractions();
+                var all_attractions = this.searchIndoorAttractions();
                 var possibleattractions = [];
                 //go through used attractions and if it is not in the all attractions list, add it to the possible attractions list
                 for (var j = 0; j < all_attractions.length; j++) {
@@ -170,27 +238,24 @@ async function getweather() {
                 
                 
             } else if (preferences == "Indoor") {
-                searchIndoorAttractions();
+                this.searchIndoorAttractions();
             } else if (preferences == "Both") {
-                searchBothAttractions();
+                this.searchBothAttractions();
             } else {
-                searchOutdoorAttractions();
+                this.searchOutdoorAttractions();
             }
         }
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
-}
-
-function sliderChange(val) {
-    document.getElementById('sliderVal').innerHTML = val;
-    }
+},
 
 
-        // Replace with your API key
 
-    // Function to search for attractions in a city
-    function searchIndoorAttractions(city) {
+
+
+    //   to search for attractions in a city
+    SearchIndoorAttractions(city) {
     var city = document.getElementById("country").value;
 
     const request = {
@@ -220,10 +285,10 @@ function sliderChange(val) {
         console.error(`Error: ${status}`);
         }
     });
-    }
+    },
 
-    function searchAttractions(city) {
-    var city = document.getElementById("country").value;
+      searchAttractions(city) {
+    var city = this.town;
 
     const request = {
         query: `Tourist Attractions in ${city}`,
@@ -252,10 +317,10 @@ function sliderChange(val) {
         console.error(`Error: ${status}`);
         }
     });
-    }
+    },
 
 
-    function searchOutdoorAttractions(city) {
+      searchOutdoorAttractions(city) {
     var city = document.getElementById("country").value;
 
     const request = {
@@ -285,8 +350,8 @@ function sliderChange(val) {
         console.error(`Error: ${status}`);
         }
     });
-    }
-function SearchMuseums(city) {
+    },
+  SearchMuseums(city) {
     var city = document.getElementById("country").value;
 
     const request = {
@@ -316,9 +381,9 @@ function SearchMuseums(city) {
         console.error(`Error: ${status}`);
         }
     });
-    }
+    },
 
-    function searchShoppingMalls(city) {
+    searchShoppingMalls(city) {
     var city = document.getElementById("country").value;
 
     const request = {
@@ -348,9 +413,9 @@ function SearchMuseums(city) {
         console.error(`Error: ${status}`);
         }
     });
-    }
-nction searchgardens(city) {
-    var city = document.getElementById("country").value;
+    },
+      searchgardens(city) {
+    var city = this.town;
 
     const request = {
         query: `Gardens and parks in ${city}`,
@@ -379,6 +444,81 @@ nction searchgardens(city) {
         console.error(`Error: ${status}`);
         }
     });
+    },
+    
+    getEateriesNearby() {
+      var geocoder = new google.maps.Geocoder();
+      var postalCode = this.postalCode;
+
+      geocoder.geocode({ address: postalCode }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          var location = results[0].geometry.location;
+          var request = {
+            location: location,
+            radius: '500',
+            type: ['restaurant'],
+            componentRestrictions: { country: "sg" }
+          };
+
+          var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch(request, this.callback);
+        } else {
+          console.error('Geocode was not successful for the following reason: ' + status);
+        }
+      },
+    )},
+    callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          console.log(place);
+          this.checkOpenStatus(place.place_id, '12:00 PM'); // Change the time as needed
+        }
+      }
     }
+    ,
+    checkOpenStatus(placeId, checkTime) {
+      var request = {
+        placeId: placeId,
+        fields: ['name', 'opening_hours'],
+      };
+
+      var service = new google.maps.places.PlacesService(map); // Assuming 'map' is accessible
+
+      service.getDetails(request, (place, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          var isOpenNow = false;
+
+          if (place.opening_hours) {
+            var checkDate = new Date('2023-10-16 ' + checkTime);
+
+            var openingHours = place.opening_hours;
+            openingHours.periods.forEach((period) => {
+              if (
+                checkDate >= new Date('2023-10-16 ' + period.open.time) &&
+                checkDate <= new Date('2023-10-16 ' + period.close.time)
+              ) {
+                isOpenNow = true;
+              }
+            });
+          }
+
+          console.log(
+            place.name + ' is open at ' + checkTime + ': ' + (isOpenNow ? 'Yes' : 'No')
+          );
+        }
+      });
+    },
+    checkempty(){
+    if (!this.town || !this.sliderValue || !this.outgoing || !this.transport) {
+        window.alert
+("Please fill in all the fields!");
+      }
+    else{
+        this.generateitinerary();
+    }
+},
+  },
+};
 
 </script>
