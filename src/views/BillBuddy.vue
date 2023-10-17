84705e840a18fabc7641121cc1a62370523d86d8
Jacobhwc <jacob.hoe.2022@scis.smu.edu.sg>
2023-10-17 23:12:20 +0800

    navbar styling + add trip page
    touched up navbar styling + some router stuff

--- src/views/BillBuddy.vue
+++ src/views/BillBuddy.vue
@@ -1,13 +1,11 @@
 <template>
   <title>BillBuddy</title>
-
 <body>
-
-  <div style="margin-left: 30px; margin-top: 30px;">
+  <div style="margin-left: 30px;">
     <h1>Current Trips</h1>
     <h7>No trips at the moment</h7>
     <br><br>
-    <router-link to="/addtripp"> Add a Trip Now! </router-link>
+    <router-link to="/add-trip"> Add a Trip Now! </router-link>
   </div>
 
 </body>
---
+++ src/views/addtrip.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h1>Add Trip</h1>
+        <form @submit.prevent="submitForm">
+            <label for="destination">Destination:</label>
+            <input type="text" id="destination" v-model="destination" required>
+            <br><br>
+            <label for="start-date">Start Date:</label>
+            <input type="date" id="start-date" v-model="startDate" required>
+            <br><br>
+            <label for="end-date">End Date:</label>
+            <input type="date" id="end-date" v-model="endDate" required>
+            <br><br>
+            <h2>Expenses:</h2>
+            <div v-for="(expense, index) in expenses" :key="index">
+                <label>Expense {{ index + 1 }}:</label>
+                &nbsp;
+                <input type="text" placeholder="Expense Name" v-model="expense.name" required>
+                &nbsp;
+                <input type="number" placeholder="Amount Spent" v-model="expense.amount" required>
+                &nbsp;                
+                <button type="button" @click="removeExpense(index)">Remove</button>
+            </div>
+            <button type="button" @click="addExpense">Add Expense</button>
+            <br><br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            destination: '',
+            startDate: '',
+            endDate: '',
+            expenses: [{ name: '', amount: '' }]
+        }
+    },
+    methods: {
+        addExpense() {
+            this.expenses.push({ name: '', amount: '' })
+        },
+        removeExpense(index) {
+            this.expenses.splice(index, 1)
+        },
+        submitForm() {
+            const trip = {
+                destination: this.destination,
+                startDate: this.startDate,
+                endDate: this.endDate,
+                expenses: this.expenses
+            }
+            console.log(trip) // replace with your own logic to submit the form data
+        }
+    }
+}
+</script>
