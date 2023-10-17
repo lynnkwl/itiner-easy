89e0aaeefa4d8b64f9260faa154a570020c62e85
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2023-10-17 23:15:46 +0800

    add form for testing database in billbuddy.vue

--- src/views/BillBuddy.vue
+++ src/views/BillBuddy.vue
@@ -7,9 +7,31 @@
     <h1>Current Trips</h1>
     <h7>No trips at the moment</h7>
     <br><br>
-    <router-link to="/addtripp"> Add a Trip Now! </router-link>
+    <router-link to="/addtrip"> Add a Trip Now! </router-link>
   </div>
 
+  <div>
+    <!-- testing database using books -->
+
+    <form class="add">
+      <label for="title">Title:</label>
+      <input type="text" name="title" required>
+      <label for="author">Author:</label>
+      <input type="text" name="author" required>
+
+      <button>add a new book</button>
+    </form>
+
+    <form class="delete">
+      <label for="id">Document id:</label>
+      <input type="text" name="id" required>
+
+      <button>delete a book</button>
+    </form>
+
+
+    
+  </div>
 </body>
 
 </template>
