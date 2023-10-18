e3049cea2599f0bf7335c5b0868240e7d1bd70d1
Jacobhwc <jacob.hoe.2022@scis.smu.edu.sg>
2023-10-18 16:55:59 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 //popupwindow
-
+import Buefy from 'buefy';
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
