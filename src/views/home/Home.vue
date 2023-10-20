d2464e609a7795aff831369e73398ba8410cb101
HunchoQuavo888 <ralph.ong.888@gmail.com>
2023-10-20 17:41:34 +0800

    Update Home.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="eye-scroll">
+    <div class="eye-scroll" style="background-color: white;">
 
       <div class="eye-container">
         <Eye></Eye>
       </div>
-
+      <div class="blank-space" style="height: 5000px;"></div>
     </div>
   </template>
   
