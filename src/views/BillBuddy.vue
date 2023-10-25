32bd32b16d51e5ca9dc565d873b8a8c94fdf868c
lynnkwl <143063371+lynnkwl@users.noreply.github.com>
2023-10-25 23:27:29 +0800

    Update BillBuddy.vue
    updated billbuddy

--- src/views/BillBuddy.vue
+++ src/views/BillBuddy.vue
@@ -102,6 +102,14 @@
   <div class="text-3xl m-7 font-bold">
     <a>Trip's name expenses</a>
   </div>
+  <div class="text-xl lm-7 drop-shadow-md">
+    <div class="relative z-0">
+      <input type="text" id="floating_standard" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+      <label for="floating_standard" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Floating standard</label>
+    </div>
+
+
+  </div>
 </template>
 
 
